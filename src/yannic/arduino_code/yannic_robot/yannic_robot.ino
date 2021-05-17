b162c701751400c11dc77bb1bc5af16f633fc3cf
Yannic Scholtyssek <yscholty@aol.com>
2021-05-17 13:06:36 +0200

    check_motor.launch now works correctly with urdf

--- src/yannic/arduino_code/yannic_robot/yannic_robot.ino
+++ src/yannic/arduino_code/yannic_robot/yannic_robot.ino
@@ -8,7 +8,6 @@
 #include <ros.h>
 #include <std_msgs/UInt16.h>
 #include <sensor_msgs/JointState.h>
-#include <std_msgs/String.h>
 
 ros::NodeHandle  nh;
 Servo rotate_base;
@@ -26,11 +25,13 @@ double gripper_joint_angle = 0;
 // function call -> processing joint_states and controlling servos
 void servo_cb(const sensor_msgs::JointState& cmd_msg){
 //get the values from joint_states topic
-rotate_base_angle = radiansToDegrees(cmd_msg.position[0]);
-joint1_angle = radiansToDegrees(cmd_msg.position[1]);
-joint2_angle = radiansToDegrees(cmd_msg.position[2]);
-joint3_angle = radiansToDegrees(cmd_msg.position[3]);
-gripper_joint_angle = radiansToDegrees(50*cmd_msg.position[4]);
+rotate_base_angle = radiansToDegrees(-1*cmd_msg.position[0]);
+joint1_angle = radiansToDegrees( -1*cmd_msg.position[1]+0.04 );
+joint2_angle = radiansToDegrees(cmd_msg.position[2]-1.04);
+joint3_angle = radiansToDegrees( -1*cmd_msg.position[3] + 0.08 );
+gripper_joint_angle = radiansToDegrees(-100*cmd_msg.position[4]+2.5 );
+
+//map the values to the correct values measuring the center point
 
 
 //write the values from joint_states to the connected Servos
@@ -45,21 +46,11 @@ gripper_joint.write(gripper_joint_angle);
 //set up the ROS node -> servo_cb is subscriber for joint_states
 ros::Subscriber<sensor_msgs::JointState> sub("joint_states", servo_cb);
 
-sensor_msgs::JointState robot_state;
-ros::Publisher pub("joint_feedback", &robot_state);
-
-char robot_id = "arm";
-char *joint_name[3] = {"joint_01", "joint_02", "joint_03"};//, "joint_04", "joint_05", "joint_06"};
-float pos[3];
-float vel[3];
-float eff[3];
-
 
 void setup() {
   nh.getHardware()->setBaud(115200);
   nh.initNode();
   nh.subscribe(sub);
-  nh.advertise(pub);
 
 //Define the servos for the joints
   rotate_base.attach(8);
@@ -75,33 +66,10 @@ void setup() {
   joint3.write(90);
   gripper_joint.write(0);
 
- 
- 
-
 }
 
 void loop() {
-  // Fulfill the sensor_msg/JointState msg
-  robot_state.name_length = 3;
-  robot_state.velocity_length = 3;
-  robot_state.position_length = 3;
-  robot_state.effort_length = 3;
-
-  pos[0] = analogRead(A0);
-  pos[1] = analogRead(A1);
-  pos[2] = analogRead(A2);
-  //pos[3] = analogRead(A3);
-
-  robot_state.header.stamp = nh.now();
-  robot_state.header.frame_id = robot_id;
-  robot_state.name = joint_name;
-  robot_state.position = pos;
-  robot_state.velocity = vel;
-  robot_state.effort = eff;
-  
-  pub.publish( &robot_state);
   nh.spinOnce();
-  delay(20);
 }
 
 // convert the radians to degree angles for the servo
