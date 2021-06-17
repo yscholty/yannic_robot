3897c99b86457f735644ed22358e6e77927376e2
Yannic_UPC <you@example.com>
2021-06-17 13:14:04 +0200

    ros_control first try

--- src/yannic/yannic_robot/src/robot_hardware_interface_node.cpp
+++ src/yannic/yannic_robot/src/robot_hardware_interface_node.cpp
@@ -1,156 +1,150 @@
-#include <yannic_robot/robot_hardware_interface.h>
+#include "ros/ros.h"
 #include "sensor_msgs/JointState.h"
+#include <mutex>
+#include <iostream>
+#include <map>
+#include <vector>
 
-void arrayCallback(const sensor_msgs::JointState::ConstPtr& joint_states);
+#include <hardware_interface/joint_state_interface.h>
+#include <hardware_interface/joint_command_interface.h>
+#include <hardware_interface/robot_hw.h>
+#include <joint_limits_interface/joint_limits.h>
+#include <joint_limits_interface/joint_limits_interface.h>
+#include <joint_limits_interface/joint_limits_rosparam.h>
+#include <joint_limits_interface/joint_limits_urdf.h>
+#include <controller_manager/controller_manager.h>
+#include <angles/angles.h>
+#include <rospy_tutorials/Floats.h>
+
+
 typedef std::map<std::string, double> MapType;
-MapType jointpositions;
-MapType jointvelocities;
 
+class ROBOTHardwareInterface  : public hardware_interface::RobotHW  {
 
-
-
-class listener
-{
 public:
-	void arrayCallback(const sensor_msgs::JointState::ConstPtr& msg);
-};
+  
+  ROBOTHardwareInterface() {}
 
+  bool init(ros::NodeHandle &root_nh, ros::NodeHandle &robot_hw_nh){
+    joint_state_sub_ = root_nh.subscribe(
+        "joint_feedback", 1000, &ROBOTHardwareInterface::arrayCallback, this);
+          pub = robot_hw_nh.advertise<rospy_tutorials::Floats>("/joints_to_arduino",10);
+          controller_manager_.reset(new controller_manager::ControllerManager(this, robot_hw_nh));
 
-
-void listener::arrayCallback(const sensor_msgs::JointState::ConstPtr& msg)
-{
-	MapType::iterator iter = jointpositions.begin();
-	for(unsigned int i = 0; i < msg->name.size(); i++)
-	{
-		
-		jointpositions[msg->name[i]] = msg -> position[i];
-			}
- 
-    //double joint_position_[5];
-	/*joint_position_[0]=angles::from_degrees(jointpositions[0]);
-	joint_position_[1]=angles::from_degrees(jointpositions[1]);
-	joint_position_[2]=angles::from_degrees(jointpositions[2]);
-	joint_position_[3]=angles::from_degrees(jointpositions[3]);
-	joint_position_[4]=angles::from_degrees(jointpositions[4]);*/
-	
-
-return;
-}
-
-
-
-ROBOTHardwareInterface::ROBOTHardwareInterface(){
-}
-
-ROBOTHardwareInterface::~ROBOTHardwareInterface() {
-}
-
-bool ROBOTHardwareInterface::init(ros::NodeHandle& root_nh, ros::NodeHandle& robot_hw_nh) {
-    
-    /*
-    num_joints_=5;
+        
+        num_joints_=5;
 	joint_names_[0]="joint1";	
 	joint_names_[1]="joint2";
 	joint_names_[2]="joint3";
 	joint_names_[3]="rotate_base";
-	joint_names_[4]="gripper_joint";*/
-	
-	//get joint names and num of joint
-    robot_hw_nh.getParam("joints", joint_name);
-    num_joints = joint_name.size();
+	joint_names_[4]="gripper_joint";
 
-    //resize vectors
-    joint_position_state.resize(num_joints);
-    joint_velocity_state.resize(num_joints);
-    joint_effort_state.resize(num_joints);
-    joint_position_command.resize(num_joints);
-    
-    joints_pub.resize(num_joints);
-	
+for (int i = 0; i < num_joints_; ++i) {
 
-    for (int i = 0; i < num_joints; ++i) {
-
-        // Create joint state interface
-        hardware_interface::JointStateHandle jointStateHandle(joint_name[i], &joint_position_state[i], &joint_velocity_state[i], &joint_effort_state[i]);
-        joint_state_interface.registerHandle(jointStateHandle);
+         // Create joint state interface
+        hardware_interface::JointStateHandle jointStateHandle(joint_names_[i], &joint_position_state_[joint_names_[i]], &joint_velocity_[i], &joint_effort_[i]);
+        joint_state_interface_.registerHandle(jointStateHandle);
+        
+        registerInterface(&joint_state_interface_);
 
         // Create position joint interface
-        hardware_interface::JointHandle jointPositionHandle(jointStateHandle, &joint_position_command[i]);
+        hardware_interface::JointHandle jointPositionHandle(jointStateHandle, &joint_position_command_[i]);
         
-        position_joint_interface.registerHandle(jointPositionHandle);
+        position_joint_interface_.registerHandle(jointPositionHandle);
    
+   
+   registerInterface(&position_joint_interface_);
     }
-    registerInterface(&joint_state_interface);
-    registerInterface(&position_joint_interface);
-    
     return true;
-}
+  }
 
-
-void ROBOTHardwareInterface::read(const ros::Time& time, const ros::Duration& period) {
-
-    //get the values from the topic "joint_states"  that were made accessibkle through the class method
-    // joint_states_sub is the object for the class listener
+  void read(const ros::Time &time, const ros::Duration &period) {
+    std::unique_lock<std::mutex> lck(joint_mtx_);
+    for (const auto name : joint_names_) {
+      ROS_INFO_STREAM(name << " " << joint_position_state_[name]);
+    }
+  }
+  
+  void write(const ros::Time &time, const ros::Duration &period) {
     
-   //sub = nh_.subscribe("joint_states", 100, arrayCallback); 
-   //joint_position_state = jointpositions;
-   //ROS_INFO("%.2f",joint_states_sub.jointpositions);
-   //ROS_INFO("Receiving  j1: %.2f, j2: %.2f, j3: %.2f, j4: %.2f,j5: %.2f",joint_position_state[0],joint_position_state[1], joint_position_state[2] , joint_position_state[3],joint_position_state[4]);
-   
-}
-
-
-void ROBOTHardwareInterface::write(const ros::Time& time, const ros::Duration& period) {
-    
-	//joints_pub.data.clear();
-	//joints_pub[0] = 90+(angles::to_degrees(joint_position_command[0]));
-	/*joints_pub.data.push_back(90+(angles::to_degrees(joint_position_command[0])));
-	joints_pub.data.push_back(90+(angles::to_degrees(joint_position_command[1])));
-	joints_pub.data.push_back(90+(angles::to_degrees(joint_position_command[2])));
-	joints_pub.data.push_back(90+(angles::to_degrees(joint_position_command[3])));
-	joints_pub.data.push_back(90+(angles::to_degrees(joint_position_command[4])));*/
-	//ROS_INFO("Publishing j1: %.2f, j2: %.2f, j3: %.2f,j4: %.2f",joints_pub.data[0],joints_pub.data[1],joints_pub.data[2], joints_pub.data[3]);
-	//pub.publish(joints_pub);	
+	joints_pub.data.clear();
+	joints_pub.data.push_back(90-(angles::to_degrees(joint_position_command_[0])));
+	joints_pub.data.push_back(90+(angles::to_degrees(joint_position_command_[1])));
+	joints_pub.data.push_back(90+(angles::to_degrees(joint_position_command_[2])));
+	joints_pub.data.push_back(90+(angles::to_degrees(joint_position_command_[3])));
+	joints_pub.data.push_back(90+(angles::to_degrees(joint_position_command_[4])));
+	//ROS_INFO("Publishing j1: %.2f, j2: %.2f, j3: %.2f, j4: %.2f, j5: %.2f",joints_pub.data[0],joints_pub.data[1],joints_pub.data[2],joints_pub.data[3],joints_pub.data[4]);
+	
+	ROS_INFO("Publishing j1: %.2f, j2: %.2f, j3: %.2f, j4: %.2f, j5: %.2f",joint_position_command_[0],joint_position_command_[1],joint_position_command_[2],joint_position_command_[3],joint_position_command_[4]);
+	pub.publish(joints_pub);	
 }
 
 
 
-int main(int argc, char** argv)
-{
-    ros::init(argc, argv, "robot_hardware_interface");
-    ros::NodeHandle nh;
-    
-    //the subscriber to the joint_statres topic
-    listener joint_states_sub;
-    ros::Subscriber sub = nh.subscribe("joint_states",1000, &listener::arrayCallback, &joint_states_sub);
-    //ros::Publisher pub;
-    
-    /* nodes defined in the Arduino code */
-	//pub = nh.advertise<rospy_tutorials::Floats>("/joints_to_arduino",10);
-    //ros::Subscriber sub;
-    ros::AsyncSpinner spinner(2);// 2 threads for controller service and for the Service client used to get the feedback from arduino
-    spinner.start();
-    
-    ROBOTHardwareInterface ROBOT;
-    bool init_success = ROBOT.init(nh,nh);
+  void arrayCallback(const sensor_msgs::JointState::ConstPtr &msg) {
+    std::unique_lock<std::mutex> lck(joint_mtx_);
+    for (unsigned int i = 0; i < msg->name.size(); i++) {
+      joint_position_state_[msg->name[i]] = msg->position[i];
+    }
+  }
 
-    
-    //ros::spin();
-    
-     controller_manager::ControllerManager cm(&ROBOT,nh);
-     
-    ros::Duration period(1.0/10); // 10Hz update rate
-    while(ros::ok()){
-        ROBOT.read(ros::Time::now(), period);
-        	ROS_INFO("Readiiiing");
-        cm.update(ros::Time::now(), period);
-        	ROS_INFO("controlllling");
-        ROBOT.write(ros::Time::now(), period);
-        	ROS_INFO("writiiing");
-        period.sleep();
-        }
-        spinner.stop();
+  ~ROBOTHardwareInterface(){
+
+  };
+
+private:
+
+  std::string joint_names_[5];
+  std::mutex joint_mtx_;
+  int num_joints_;
+  ros::Subscriber joint_state_sub_;
+  ros::Publisher pub;
+
+  
+  double joint_velocity_[5];
+  double joint_effort_[5];
+  double joint_position_command_[5];
+  rospy_tutorials::Floats joints_pub;
+  boost::shared_ptr<controller_manager::ControllerManager> controller_manager_;
+
+  MapType joint_position_state_;
+  
+  hardware_interface::JointStateInterface joint_state_interface_;
+  hardware_interface::PositionJointInterface position_joint_interface_;
+  joint_limits_interface::PositionJointSaturationInterface position_joint_saturation_interface_;
+  joint_limits_interface::PositionJointSoftLimitsInterface positionJointSoftLimitsInterface;
         
-    return 0;
-}
+};
 
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "robot_hardware_interface");
+  ros::NodeHandle nh;
+  
+  
+  
+  
+
+
+  ros::AsyncSpinner spinner(
+      2); // 2 threads for controller service and for the Service client used to
+          // get the feedback from arduino
+  spinner.start();
+
+  ROBOTHardwareInterface ROBOT;
+  controller_manager::ControllerManager cm(&ROBOT);
+  bool init_success = ROBOT.init(nh, nh);
+
+  ros::Duration period(1.0 / 10); // 10Hz update rate
+  while (ros::ok()) {
+    ROS_INFO("Readiiiing");
+    ROBOT.read(ros::Time::now(), period);
+    ROS_INFO("Updatiiiing");
+    cm.update(ros::Time::now(), period);
+    ROS_INFO("Writiiiiing");
+    ROBOT.write(ros::Time::now(), period);
+    period.sleep();
+  }
+  spinner.stop();
+
+  return 0;
+}
